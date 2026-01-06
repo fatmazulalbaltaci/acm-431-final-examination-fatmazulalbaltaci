95ea04d915236ca5c7800484eaa2fcfee49a53a2
Fatma Zülal Baltacı <152783085+fatmazulalbaltaci@users.noreply.github.com>
2026-01-06 16:13:16 +0300

    Update TaskRepository.kt

--- app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
+++ app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
@@ -7,7 +7,16 @@ class TaskRepository(private val api: TaskApi) {
 
     suspend fun fetchTasks(): List<Task> {
         // TODO 1: Call API
+        val taskDtos = api.getTasks()
         // TODO 2: Convert TaskDto list to Task list
-        return TODO("Provide the return value")
+        return taskDtos.map { dto ->
+            Task(
+                id = dto.id,
+                title = dto.title,
+                isCompleted = dto.completed
+            )
+        }
     }
 }
+
+
