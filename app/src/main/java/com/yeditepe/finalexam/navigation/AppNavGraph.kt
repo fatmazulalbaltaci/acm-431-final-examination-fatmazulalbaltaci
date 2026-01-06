53c412c5295f1c843d53a7e2430280890a810386
Fatma Zülal Baltacı <152783085+fatmazulalbaltaci@users.noreply.github.com>
2026-01-06 15:48:10 +0300

    Update AppNavGraph.kt

--- app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
+++ app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
@@ -1,8 +1,18 @@
 package com.yeditepe.finalexam.navigation
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable // Bu import gereklidir
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.gcm.Task
+import com.yeditepe.finalexam.ui.TaskDetailScreen
+import com.yeditepe.finalexam.ui.TaskListScreen
 
 @Composable
 fun AppNavGraph() {
@@ -15,7 +25,40 @@ fun AppNavGraph() {
     ) {
 
         // TODO 1: Add composable for "tasks"
+        composable("tasks") {
+
+            TaskListScreen(navController = navController)
+        }
 
         // TODO 2: Add composable for "taskDetail/{title}"
+        composable("taskDetail/{title}") { backStackEntry ->
+
+            val title = backStackEntry.arguments?.getString("title") ?: ""
+            TaskDetailScreen(title = title)
+        }
     }
 }
+
+@Composable
+fun TaskListScreen(navController: NavController) {
+    @Composable
+    fun TaskRow(task: Task, navController: NavController) {
+        Text(
+            text = task.title,
+            modifier = Modifier.clickable {
+                // TODO 3: Navigate to detail screen with task title [cite: 91]
+                navController.navigate("taskDetail/${task.title}")
+            }
+        )
+    }
+
+    @Composable
+    fun TaskDetailScreen(title: String) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            // TODO 4: Display task title [cite: 103]
+            Text(text = "Task Detail: $title")
+        }
+    }
+}
+
+
